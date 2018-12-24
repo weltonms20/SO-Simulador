68fd70866d6b9d532d8a7ba604fbdc590f3d62bf
welton <welton457@gmail.com>
2018-12-24 10:26:00 -0300

    [MOD] modificado sistema de filas (mais completo)

--- Simulador/main.lua
+++ Simulador/main.lua
@@ -11,33 +11,33 @@ function love.load()
 	----------------------------------------------------------------------------------------
 
 	fila={}
-	fila[#fila+1] = processos[1]
-	fila[#fila+1] = processos[2]
+	adiciona_fila(processos[1])
+	adiciona_fila(processos[2])
 
 	if(#fila>0)then
-		atual = fila[1]
+		atual = primeiro_fila()
 	else
 		atual=0--significa nao fazer nada
 	end
 	if(#fila>1)then
-		proximo = fila[2]
+		espera = espera_fila()
 	else
-		proximo = 0--zero significa nao fazer nada
+		espera = 0--zero significa nao fazer nada
 	end
 	----------------------------------------------------------------------------------------
 	tempo = os.time()
 	numero_random= love.math.random(1,10)
-	auxcpu = 0 -- auxiliar de cotagem de tempo cpu-bound
-	auxio = 0 -- auxiliar de contagem de tempo io-bound
+	tempo_cpu = 0 -- auxiliar de cotagem de tempo cpu-bound
+	tempo_io = 0 -- auxiliar de contagem de tempo io-bound
 
 end
 
 function love.update( dt )
 	escalonamento_loteria()
 	if(atual~=0 and processos[atual].tipo == "cpu-bound")then
-		auxcpu = auxcpu+1/50 -- tempo que cada processo e executado
+		tempo_cpu = tempo_cpu+1/50 -- tempo que cada processo e executado
 	else
-		auxio = auxio+1/50
+		tempo_io = tempo_io+1/50
 	end
 	-- body
 end
@@ -46,17 +46,17 @@ function love.draw(  )
 	-- body
 	if(atual~=0 and processos[atual].tipo == "cpu-bound")then
 		love.graphics.print("\ncpu-bound executando".."\n".."PID: "..processos[atual].pid.."\n")
-		love.graphics.print("temp CPU: ".. auxcpu.."\n")
+		love.graphics.print("temp CPU: ".. tempo_cpu.."\n")
 		
 	else
 		love.graphics.print("\nio-bound executando\n".."PID: "..processos[atual].pid.."\n")
-		love.graphics.print("temp CPU: "..auxio)
+		love.graphics.print("temp CPU: "..tempo_io)
 	end
 	
 end
 
 function sorteio()
-	aux = love.math.random(10)
+	local aux = love.math.random(10)
 	return aux
 end
 
@@ -65,17 +65,13 @@ function escalonamento_rrobin() -- funcao escalonador round-robin
 		if(os.time()-tempo>0.4) then -- tempo que o I/O fica executando na CPU
 			tempo = os.time() -- quando o tempo termina a variavel tempo e atualizada 
 			
-			temp = atual
-			atual = espera -- variavel atual muda
-			espera = temp 
+			proximo_fila()
 		end
 	else
 		if(os.time() - tempo>5) then -- tempo que o CPU-Bound fica executando na CPU
 			tempo = os.time()
 
-			temp = atual
-			atual = espera -- variavel atual muda
-			espera = temp 
+			proximo_fila()
 		end
 	end
 end
@@ -83,60 +79,47 @@ end
 function escalonamento_prioridades()
 	if(processos[atual].tipo == "io-bound")then
 		if(processos[atual].prioridade > processos[espera].prioridade)then
-			temp = atual --variavel temporaria
-			atual = espera	-- isso tem que ser mudado pois nao posso passar o indice aqui
-							-- esse indice tem que vir de algum lugar (alguma funcao tem que dizer qual esse indice)
-			espera = temp -- tem que ser mudado teambem , ttemos que ter uma fila
+			proximo_fila()
 		else
 			if(processos[atual].prioridade <= processos[espera].prioridade)then
 				if(os.time()-tempo>0.4)then
 					tempo =os.time()
-					temp = atual
-					atual = espera
-					espera = temp
+					proximo_fila()
 				end
 			end
 		end
 	else
 		if(os.time()-tempo>5)then
 			tempo = os.time()
-			atual = "io-bound"
+			proximo_fila()
 		end
 	end
 end
 
 function escalonamento_loteria()
 -- body
-	atual = 1
-	if (processos[atual].token[aux]) then
-		atual = 1
-		love.graphics.print("\n\n\nToken sorteado: "..aux)
+	if (processos[atual].token[numero_random]) then
+		love.graphics.print("\n\n\nToken sorteado: "..numero_random)
 		if(os.time()-tempo>5)then
 			tempo = os.time()
 
-			temp = atual
-			atual = espera
-			espera = temp
+			proximo_fila()
 
 			aux = love.math.random(10)
 		end
 
-	elseif(processos[espera].token[aux])then
-			temp = atual
-			atual = espera
-			espera = temp
-			love.graphics.print("\n\n\nToken sorteado: "..aux)
+	elseif(processos[espera].token[numero_random])then
+			proximo_fila()
+			love.graphics.print("\n\n\nToken sorteado: "..numero_random)
 			if(os.time()-tempo>1)then
 				tempo=os.time()
 
-				temp = atual
-				atual = espera
-				espera = temp
+				proximo_fila()
 
-				aux = love.math.random(10)
+				numero_random = love.math.random(10)
 			end
 	else
-		aux = love.math.random(10)
+		numero_random = love.math.random(10)
 	end
 end
 
@@ -150,13 +133,13 @@ end
 
 -------------------------SISTEMA DE FILA--------------------------
 function troca_fila(nodeA, nodeB)
-	self.fila[nodeA],self.fila[nodeB] = self.fila[nodeB],self.fila[nodeA]
+	fila[nodeA],fila[nodeB] = fila[nodeB],fila[nodeA]
 end
 function adiciona_fila(node)
-	self.fila[#fila+1] = node
+	fila[#fila+1] = node
 end
 function remove_fila(indice)
-	self.fila.remove(indice)
+	fila.remove(indice)
 end
 function imprime_fila()
 	for i=1,#fila do
@@ -182,5 +165,16 @@ function imprimeNode_fila(indice)
 		print("imprimindo indice null")
 	end
 end
-
+function proximo_fila()
+	local temp = fila[1]
+	adiciona_fila(temp)--coloca no final da fila
+	remove_fila(1)--remove o primeiro da fila
+	return fila[1] --retorna o proximo
+end
+function primeiro_fila()
+	return 1
+end
+function espera_fila()
+	return 2
+end
 
